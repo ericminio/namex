886c3e83c089224d05ecd514dd49d28eaf07cd70
Eric Mignot <eric.mignot@gmail.com>
2018-11-07 14:14:47 -0800

    first exact match against possible.conflicts

--- api/namex/resources/exact_match.py
+++ api/namex/resources/exact_match.py
@@ -1,9 +1,11 @@
-from flask import jsonify
+from flask import jsonify, request
 from flask_restplus import Resource, Namespace
 import requests
 import json
 
 api = Namespace('exactMatchMeta', description='Exact Match System - Metadata')
+import os
+SOLR_URL = os.getenv('SOLR_BASE_URL')
 
 
 @api.route("")
@@ -11,7 +13,13 @@ class ExactMatch(Resource):
 
     @staticmethod
     def get():
-        url = 'http://localhost:8983/solr/exact_match/select?q=*:*&wt=json'
+        query = request.args.get('query')
+        url = SOLR_URL + '/solr/possible.conflicts' + \
+              '/select?' + \
+              'sow=false' + \
+              '&df=name_exact_match' + \
+              '&wt=json' + \
+              '&q=' + query
         results = requests.get(url)
         text = results.text
         answer = json.loads(text)
--- api/tests/python/end_points/test_exact_match.py
+++ api/tests/python/end_points/test_exact_match.py
@@ -1,4 +1,7 @@
 from namex.models import User
+import os
+import requests
+import json
 
 token_header = {
                 "alg": "RS256",
@@ -23,50 +26,139 @@ claims = {
                 ]
             }
          }
+SOLR_URL = os.getenv('SOLR_TEST_URL')
 
+def test_solr_available(app, client, jwt):
+    url = SOLR_URL + '/solr/possible.conflicts/admin/ping'
+    r = requests.get(url)
 
-
+    assert r.status_code == 200
 
 def clean_database(client, jwt):
-    import requests
-    url = 'http://localhost:8983/solr/exact_match/update?commit=true'
+    url = SOLR_URL + '/solr/possible.conflicts/update?commit=true'
     headers = {'content-type': 'text/xml'}
     data = '<delete><query>id:*</query></delete>'
     r = requests.post(url, headers=headers, data=data)
+
     assert r.status_code == 200
 
 def seed_database_with(client, jwt, name):
-    import requests
-    url = 'http://localhost:8983/solr/exact_match/update?commit=true'
+    clean_database(client, jwt)
+    url = SOLR_URL + '/solr/possible.conflicts/update?commit=true'
     headers = {'content-type': 'application/json'}
-    data = '[{"name":"' + name + '", "id":"1"}]'
+    data = '[{"source":"CORP", "name":"' + name + '", "id":"1"}]'
     r = requests.post(url, headers=headers, data=data)
+
     assert r.status_code == 200
 
 def verify(data, expected):
     print(data['names'])
+
     assert expected == data['names']
 
 def verify_exact_match_results(client, jwt, query, expected):
     data = search_exact_match(client, jwt, query)
     verify(data, expected)
 
-
 def search_exact_match(client, jwt, query):
-    import json
     token = jwt.create_jwt(claims, token_header)
     headers = {'Authorization': 'Bearer ' + token}
-    rv = client.get('/api/v1/exact-match', headers=headers)
-    assert rv.status_code == 200
-    data = json.loads(rv.data)
-    return data
+    rv = client.get('/api/v1/exact-match?query='+query, headers=headers)
 
-def test_exact_match(client, jwt, app):
-    clean_database(client, jwt)
-    seed_database_with(client, jwt, 'JM Van Damme Ltd')
+    assert rv.status_code == 200
+    return json.loads(rv.data)
+
+def test_find_same_name(client, jwt, app):
+    seed_database_with(client, jwt, 'JM Van Damme inc')
     verify_exact_match_results(client, jwt,
-        query='JM Van Damme Inc',
+        query='JM Van Damme inc',
         expected=[
-            { 'name':'JM Van Damme Ltd' }
+            { 'name':'JM Van Damme inc' }
         ]
     )
+
+def test_resists_different_type(client, jwt, app):
+    seed_database_with(client, jwt, 'JM Van Damme inc')
+    verify_exact_match_results(client, jwt,
+        query='JM Van Damme ltd',
+        expected=[
+            { 'name':'JM Van Damme inc' }
+        ]
+    )
+
+def test_case_insensitive(client, jwt, app):
+    seed_database_with(client, jwt, 'JM Van Damme inc')
+    verify_exact_match_results(client, jwt,
+        query='JM VAN DAMME INC',
+        expected=[
+            { 'name':'JM Van Damme inc' }
+        ]
+    )
+
+def test_no_match(client, jwt, app):
+    seed_database_with(client, jwt, 'JM Van Damme inc')
+    verify_exact_match_results(client, jwt,
+        query='Hello BC inc',
+        expected=[]
+    )
+
+def test_ignores_and(client, jwt, app):
+    seed_database_with(client, jwt, 'JM Van Damme inc')
+    verify_exact_match_results(client, jwt,
+       query='J and M Van Damme inc',
+       expected=[
+           {'name': 'JM Van Damme inc'}
+       ]
+    )
+
+def test_ignores_dots(client, jwt, app):
+    seed_database_with(client, jwt, 'J.M. Van Damme Inc')
+    verify_exact_match_results(client, jwt,
+       query='JM Van Damme Inc',
+       expected=[
+           {'name': 'J.M. Van Damme Inc'}
+       ]
+    )
+
+def test_ignores_ampersand(client, jwt, app):
+    seed_database_with(client, jwt, 'J&M & Van Damme Inc')
+    verify_exact_match_results(client, jwt,
+       query='JM Van Damme Inc',
+       expected=[
+           {'name': 'J&M & Van Damme Inc'}
+       ]
+    )
+
+def test_no_match_because_additional_initial(client, jwt, app):
+    seed_database_with(client, jwt, 'J.M.J. Van Damme Trucking Inc')
+    verify_exact_match_results(client, jwt,
+       query='J.M. Van Damme Trucking Inc',
+       expected=[]
+    )
+
+def test_no_match_because_additional_word(client, jwt, app):
+    seed_database_with(client, jwt, 'JM Van Damme Trucking Inc')
+    verify_exact_match_results(client, jwt,
+       query='JM Van Damme Trucking International Inc',
+       expected=[]
+    )
+
+def test_no_match_because_missing_one_word(client, jwt, app):
+    seed_database_with(client, jwt, 'JM Van Damme Physio inc')
+    verify_exact_match_results(client, jwt,
+       query='JM Van Damme inc',
+       expected=[]
+    )
+
+def test_duplicated_letters(client, jwt, app):
+    seed_database_with(client, jwt, 'Dame Trucking Inc')
+    verify_exact_match_results(client, jwt,
+       query='Damme Trucking Inc',
+       expected=[
+           {'name': 'Dame Trucking Inc'}
+       ]
+    )
+
+
+
+
